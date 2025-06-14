85a6d57b766c2c449335bceac76dc3ddb0b98a21
Hawaz <hawazt@gmail.com>
2025-06-14 15:21:44 -0400

    Fixing bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     db.init_app(app)
-    
+
+    # Register blueprints 
     from .routes import main
     app.register_blueprint(main)
     print("DB URI in use:", app.config["SQLALCHEMY_DATABASE_URI"])
+
 
     return app
--- run.py
+++ run.py
@@ -1,10 +1,8 @@
-from app import create_app,db
+from app import create_app, db
 
 app = create_app()
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
